b62e8aa77b5f659bfbf1c215915e0d1f738e42ec
Ndumiso Mguni <Ndumiso29@gmail.com>
2020-08-08 11:53:06 -0400

    server updates

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-// if (process.env.NODE_ENV !== "production") {
-//   require("dotenv").load();
-// }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const { MONGODB } = require("./config.js");
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
-const indexRouter = require("./routes/index");
-const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
-// set view engine
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
+
+// ejs is view eng
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-//layout file - everysingle file wil be but inside of this file
+// all files will be put in this layout file. less duplicates
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-// out public files. style sheets, img, js etc
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-// // mongoose
-mongoose.connect(MONGODB, {
-  useNewUrlParser: true,
-});
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () =>
-  console.log("MongoDB database connection established successfully")
-);
+db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 
 app.listen(process.env.PORT || 3000);
